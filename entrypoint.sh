679daa0d0891275ee7a50ecbc6fd902dca232e0d
Julien Rottenberg <julien@rottenberg.info>
2018-01-22 15:53:26 -0800

    manage cron better

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,19 @@
 
 set -euo pipefail
 
+rm  /etc/cron.*/*
+cp /etc/crontab /etc/cron.d/
 
 case $(echo ${CRON} | tr 'A-Z' 'a-z') in
 hourly)
-  rm  /etc/cron.*/*
   mv /backup.sh /etc/cron.hourly/backup_mongo
   cron -f
   ;;
 daily)
-  rm  /etc/cron.*/*
   mv /backup.sh /etc/cron.daily/backup_mongo
   cron -f
   ;;
 weekly)
-  rm  /etc/cron.*/*
   mv /backup.sh /etc/cron.weekly/backup_mongo
   cron -f
   ;;
